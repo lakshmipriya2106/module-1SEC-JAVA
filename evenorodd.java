787f148ac4c2ab71f5449401bc240d7a7033d2ee
Lakshmi Priya <rlakshmipriya152@gmail.com>
2025-09-02 21:22:20 +0530

    Add files via upload

---
+++ evenorodd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class evenorodd{
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number:");
+int num=sc.nextInt();
+if(num%2==0){
+    System.out.println("is an even number");
+}
+else{
+    System.out.println("is an odd number");
+}
+}
+}
